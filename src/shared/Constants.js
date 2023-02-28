7396204d6ae9653e822d79e29dcdec297259f613
Đỗ Hải Long <74364655+HaiLong9901@users.noreply.github.com>
2023-02-28 16:07:01 +0700

    update constant

--- src/shared/Constants.js
+++ src/shared/Constants.js
@@ -1,7 +1,7 @@
 import img from '../assets/Image/user.png'
 export const USER_DEFAULT_AVATAR = img
-// export const API = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api/' : 'acasc'
-export const API = process.env.NODE_ENV !== 'production' ? 'https://tasty-food-server.onrender.com/api/' : 'acasc'
+export const API =
+  process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api/' : 'https://tasty-food-server.onrender.com/api/'
 export const LOCAL_STORAGE_TOKEN_NAME = 'accessToken'
 export const LOCAL_STORAGE_USER = 'user'
 export const LOCAL_STORAGE_NUMBER_PRODUCTS_IN_CART = 'cartItems'
